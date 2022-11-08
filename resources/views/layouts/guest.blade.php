cacac4a79ec474353397d463e9b5c85a54fc238c
becardine <bealvescardine@gmail.com>
2022-11-07 23:24:50 -0300

    feat: :sparkles: create edit & delete reservations

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,9 +17,9 @@
 
 <body>
     <div class="bg-white shadow-md" x-data="{ isOpen: false }">
-        <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+        <nav class="container px-6 py-6 mx-auto md:flex md:justify-between md:items-center">
             <div class="flex items-center justify-between">
-                <a class="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-2xl hover:text-green-400" href="#">
+                <a class="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-2xl hover:text-green-400" href="/">
                     TailFood
                 </a>
                 <!-- Mobile menu button -->
