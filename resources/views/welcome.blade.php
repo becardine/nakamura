7ce592f469cca782b2151efd96fc6ed84af2b944
becardine <bealvescardine@gmail.com>
2022-11-09 23:44:08 -0300

    style: :lipstick: session about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,81 +1,35 @@
 <x-guest-layout>
     <!-- Main Hero Content -->
-    <div class="main-hero py-32 mx-auto -mt-24 text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center" style="background-image: url('{{ asset('images/banner-1.jpg') }}')">
+    <div class="main-hero py-32 mx-auto -mt-24 text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
+        style="background-image: url('{{ asset('images/banner-1.jpg') }}')">
     </div>
     <!-- End Main Hero Content -->
-    <section class="px-2 py-32 bg-white md:px-0">
-        <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+    <section class="section-about px-2 py-32 bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
+        style="background-image: url('{{ asset('images/session-about.jpg') }}')">
+        <div class="items-center max-w-6xl px-8 mx-auto xl:px-5 sm:flex sm:flex-wrap">
             <div class="flex flex-wrap items-center sm:-mx-3">
                 <div class="w-full md:w-1/2 md:px-3">
                     <div class="w-full pb-6 space-y-4 sm:max-w-md lg:max-w-lg lg:space-y-4 lg:pr-0 md:pb-0">
-                        <!-- <h1
-              class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl"
-            > -->
-                        <h3 class="text-xl">OUR STORY
-                        </h3>
-                        <h2 class="text-4xl text-green-600">Welcome</h2>
-                        <!-- </h1> -->
-                        <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo incidunt praesentium, ipsum
-                            culpa minus eveniet, id nesciunt excepturi sit voluptate repudiandae. Explicabo, incidunt quia.
-                            Repellendus mollitia quaerat est voluptas!
-                        </p>
+
                         <div class="relative flex">
-                            <a href="#_" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-green-600 rounded-md sm:mb-0 hover:bg-green-700 sm:w-auto">
-                                Read More
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <line x1="5" y1="12" x2="19" y2="12"></line>
-                                    <polyline points="12 5 19 12 12 19"></polyline>
-                                </svg>
-                            </a>
+
                         </div>
                     </div>
                 </div>
-                <div class="w-full md:w-1/2">
-                    <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                        <img src="https://cdn.pixabay.com/photo/2017/08/03/13/30/people-2576336_960_720.jpg" />
-                    </div>
+                <div class="w-full md:w-1/2 justify-end space-y-4">
+                    <h1 class="text-red-700 text-5xl text-end font-bold">Nossa História</h1>
+                    <p class="text-white text-end">
+                        Com a vinda dos imigrantes japoneses há mais de um século, vieram com eles novos hábitos de uma
+                        nova cultura e da culinária japonesa. Os pratos e itens vindos do Oriente integraram-se com a
+                        comida do Ocidente e surgiu uma diversificada alimentação.
+                    </p>
+                    <br>
+                    <p class="text-white text-end">
+                        Aqui no <span class="text-red-700">Nakaruma</span>, criamos pratos com todo cuidado e com muito sabor. Conseguindo entregar o melhor da culinária japonesa com qualidade indiscutível.
+                        Em nossos espaços você tem momentos agradáveis, um ambiente aconchegante, além é claro, do
+                        melhor atendimento e frescor dos alimentos.
+                    </p>
                 </div>
-            </div>
-        </div>
-    </section>
-    <section class="py-20 bg-gray-50">
-        <div class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
-            <div class="flex flex-wrap items-center -mx-3">
-                <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
-                    <div class="w-full lg:max-w-md">
-                        <h2 class="mb-4 text-2xl font-bold">About Us</h2>
-                        <h2 class="mb-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-                            WHY CHOOSE US?</h2>
-
-                        <p class="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Natus hic atque magni minus aliquam, eos quam incidunt nam iusto sunt voluptates
-                            inventore a veritatis doloremque corrupti. Veritatis est expedita cupiditate!</p>
-                        <ul>
-                            <li class="flex items-center py-2 space-x-4 xl:py-3">
-                                <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z">
-                                    </path>
-                                </svg>
-                                <span class="font-medium text-gray-500">Faster Processing and Delivery</span>
-                            </li>
-                            <li class="flex items-center py-2 space-x-4 xl:py-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                                <span class="font-medium text-gray-500">Easy Payments</span>
-                            </li>
-                            <li class="flex items-center py-2 space-x-4 xl:py-3">
-                                <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z">
-                                    </path>
-                                </svg>
-                                <span class="font-medium text-gray-500">100% Protection and Security for Your App</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0"><img class="mx-auto sm:max-w-sm lg:max-w-full" src="https://cdn.pixabay.com/photo/2020/12/31/12/28/cook-5876388_960_720.png" alt="feature image"></div>
             </div>
         </div>
     </section>
@@ -91,7 +45,8 @@
                     <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                         <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
                         <div class="px-6 py-4">
-                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">{{ $menu->name }}</h4>
+                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
+                                {{ $menu->name }}</h4>
                             <p class="leading-normal text-gray-700">{{ $menu->description }}</p>
                         </div>
                         <div class="flex items-center justify-between p-4">
@@ -111,22 +66,28 @@
         </div>
         <div class="container grid gap-4 mx-auto lg:grid-cols-3 md:grid-cols-3">
             <div class="w-full rounded">
-                <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="image" class="object-cover w-full h-80">
+                <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                    alt="image" class="object-cover w-full h-80">
             </div>
             <div class="w-full rounded">
-                <img src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="image" class="object-cover w-full h-80">
+                <img src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                    alt="image" class="object-cover w-full h-80">
             </div>
             <div class="w-full rounded">
-                <img src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="image" class="object-cover w-full h-80">
+                <img src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                    alt="image" class="object-cover w-full h-80">
             </div>
             <div class="w-full rounded">
-                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image" class="object-cover w-full h-80">
+                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="image" class="object-cover w-full h-80">
             </div>
             <div class="w-full rounded">
-                <img src="https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image" class="object-cover w-full h-80">
+                <img src="https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="image" class="object-cover w-full h-80">
             </div>
             <div class="w-full rounded">
-                <img src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image" class="object-cover w-full h-80">
+                <img src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                    alt="image" class="object-cover w-full h-80">
             </div>
         </div>
     </section>
@@ -140,12 +101,15 @@
         <div class="grid gap-2 lg:grid-cols-3">
             <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
                 <div class="flex justify-center -mt-16 md:justify-end">
-                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80">
+                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
+                        src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80">
                 </div>
                 <div>
                     <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt
-                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
+                        deserunt
+                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis
+                        pariatur
                         minus consequuntur!</p>
                 </div>
                 <div class="flex justify-end mt-4">
@@ -154,12 +118,15 @@
             </div>
             <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
                 <div class="flex justify-center -mt-16 md:justify-end">
-                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full" src="https://cdn.pixabay.com/photo/2018/01/04/21/15/young-3061652__340.jpg">
+                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
+                        src="https://cdn.pixabay.com/photo/2018/01/04/21/15/young-3061652__340.jpg">
                 </div>
                 <div>
                     <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt
-                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
+                        deserunt
+                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis
+                        pariatur
                         minus consequuntur!</p>
                 </div>
                 <div class="flex justify-end mt-4">
@@ -168,12 +135,15 @@
             </div>
             <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
                 <div class="flex justify-center -mt-16 md:justify-end">
-                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full" src="https://cdn.pixabay.com/photo/2018/01/18/17/48/purchase-3090818__340.jpg">
+                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
+                        src="https://cdn.pixabay.com/photo/2018/01/18/17/48/purchase-3090818__340.jpg">
                 </div>
                 <div>
                     <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt
-                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur
+                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
+                        deserunt
+                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis
+                        pariatur
                         minus consequuntur!</p>
                 </div>
                 <div class="flex justify-end mt-4">
